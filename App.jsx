4e848e4e78d51dcdfcd88fa8b673ed6dfca35ca7
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2021-03-07 17:06:18 +0530

    Add files via upload
    Made a simple to-do list using React forms. Wanted to use tailwind for styling but went ahead with classic methods.

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newItem = event.target.value;
+    setInputText(newItem);
+  }
+
+  function handleClick() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
